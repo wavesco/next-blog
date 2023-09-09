94ab66ad0cd11937fdbea00497790ebf101810c2
Jonathan Lizama <Jonathan.B.Lizama@gmail.com>
2023-09-09 13:31:06 -0500

    Added Dark Mode

--- pages/index.js
+++ pages/index.js
@@ -4,6 +4,13 @@ import utilStyles from '../styles/utils.module.css';
 import { getSortedPostsData } from '../lib/posts';
 import Link from 'next/link';
 import Date from '../components/date'
+import {Roboto} from "@next/font/google"
+import Button from '../components/button'
+const roboto = Roboto({
+  weight:"900",
+  subsets:["latin"]
+})
+
 
 export async function getStaticProps() {
   const allPostsData = getSortedPostsData();
@@ -20,6 +27,14 @@ export default function Home({ allPostsData }) {
       <Head>
         <title>{siteTitle}</title>
       </Head>
+      <main className="flex items-left justify-left h-screen flex-col">
+        <h2 className={`${roboto.className} text-4xl sm:text-6xl md:text-9xl text-center text-gray-800`}></h2>
+        <h2 className={`${roboto.className} text-4xl sm:text-6xl md:text-9xl text-center text-white `}></h2>
+        {/* <button className='bg-gray-800 dark:bg-gray-50 hover:bg-gray-600 dark:hover:bg-gray-300 transition-all duration-100 text-white dark:text-gray-800 px-8 py-2 text-2xl md:text-4xl rounded-lg absolute bottom-32'>
+          Toggle Mode
+        </button> */}
+        <Button/>
+      </main>
       <section className={utilStyles.headingMd}>
         <p>
           Hello, I'm Jonathan, and I'm a software engineer based in Houston, Texas. 
