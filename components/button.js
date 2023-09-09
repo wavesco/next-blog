13600437371faa1730cd24dcf459592f673f95eb
wavesco <Jonathan.B.Lizama@gmail.com>
2023-09-09 14:38:41 -0500

    Added Dark Mode Icon

--- components/button.js
+++ components/button.js
@@ -1,7 +1,9 @@
 "use client"
-import React from 'react'
+import React from 'react';
 import { useTheme } from "next-themes";
-
+import IconButton from '@mui/material/IconButton';
+import Brightness4Icon from '@mui/icons-material/Brightness4';
+import Brightness7Icon from '@mui/icons-material/Brightness7';
 
 const Button = () => {
     const { systemTheme, theme, setTheme } = useTheme();
@@ -9,12 +11,13 @@ const Button = () => {
     const currentTheme = theme === 'system' ? systemTheme : theme;
 
     return (
-        <button
-            onClick={() => theme == "dark"? setTheme('light'): setTheme("dark")}
-            className='bg-gray-800 dark:bg-gray-50 hover:bg-gray-600 dark:hover:bg-gray-300 transition-all duration-100 text-white dark:text-gray-800 px-8 py-2 text-2xl md:text-4xl rounded-lg absolute bottom-32'>
-            Toggle Mode
-        </button>
-    )
+        <IconButton
+            onClick={() => currentTheme === "dark" ? setTheme('light') : setTheme("dark")}
+            className='bg-gray-800 dark:bg-gray-50 hover:bg-gray-600 dark:hover:bg-gray-300 transition-all duration-100 text-white dark:text-gray-800 rounded-full p-2 absolute bottom-32 right-32'
+        >
+            {currentTheme === "dark" ? <Brightness7Icon /> : <Brightness4Icon />}
+        </IconButton>
+    );
 }
 
-export default Button
+export default Button;
