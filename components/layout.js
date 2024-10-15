57aea29024fbed3066c4b76f87272f5a2ea4a906
Jonathan <johnblizama@gmail.com>
2024-10-15 12:21:40 -0500

    Refactor layout component and update profile image

--- components/layout.js
+++ components/layout.js
@@ -1,87 +1,87 @@
-import Head from 'next/head';
-import Image from 'next/image';
-import styles from './layout.module.css';
-import utilStyles from '../styles/utils.module.css';
-import Link from 'next/link';
-import HomeIcon from '@mui/icons-material/Home';
-import IconButton from '@mui/material/IconButton';
-import Button from '../components/button'
-import { motion } from "framer-motion"
-import ScrollAnimation from './scrollanimation';
+import Head from "next/head";
+import Image from "next/image";
+import styles from "./layout.module.css";
+import utilStyles from "../styles/utils.module.css";
+import Link from "next/link";
+import HomeIcon from "@mui/icons-material/Home";
+import IconButton from "@mui/material/IconButton";
+import Button from "../components/button";
+import { motion } from "framer-motion";
+import ScrollAnimation from "./scrollanimation";
 
-const name = 'Jonathan Lizama';
-export const siteTitle = 'Portfolio';
+const name = "Jonathan Lizama";
+export const siteTitle = "Jon Lizama Portfolio";
 
 export default function Layout({ children, home }) {
   return (
-    <motion.div animate={{ opacity: [0, .5, .75, 1] }}>
-    <ScrollAnimation>
-    <div className={styles.container}>
-      <Head>
-        <link rel="icon" href="/favicon.ico" />
-        <meta
-          name="portfolio"
-          content="personal website portfolio using Next.js"
-        />
-        <meta
-          property="og:image"
-          content={`https://og-image.vercel.app/${encodeURI(
-            siteTitle,
-          )}.png?theme=light&md=0&fontSize=75px&images=https%3A%2F%2Fassets.vercel.com%2Fimage%2Fupload%2Ffront%2Fassets%2Fdesign%2Fnextjs-black-logo.svg`}
-        />
-        <meta name="og:title" content={siteTitle} />
-        <meta name="twitter:card" content="summary_large_image" />
-      </Head>
-      <main className="flex items-left justify-left h-screen flex-col">
-      <div className={styles.buttonContainer}>
-        <Button/>
-      </div>
-      </main>
-      <header className={styles.header}>
-        {home ? (
-          <>
-            <Image
-              priority
-              src="/images/profile.jpg"
-              className={utilStyles.borderCircle}
-              height={144}
-              width={144}
-              alt=""
+    <motion.div animate={{ opacity: [0, 0.5, 0.75, 1] }}>
+      <ScrollAnimation>
+        <div className={styles.container}>
+          <Head>
+            <link rel="icon" href="/favicon.ico" />
+            <meta
+              name="portfolio"
+              content="personal website portfolio built using Next.js"
             />
-            <h1 className={utilStyles.heading2Xl}>{name}</h1>
-          </>
-        ) : (
-          <>
-            <Link href="/">
-              <Image
-                priority
-                src="/images/profile.jpg"
-                className={utilStyles.borderCircle}
-                height={108}
-                width={108}
-                alt=""
-              />
-            </Link>
-            <h2 className={utilStyles.headingLg}>
-              <Link href="/" className={utilStyles.colorInherit}>
-                {name}
+            <meta
+              property="og:image"
+              content={`https://og-image.vercel.app/${encodeURI(
+                siteTitle
+              )}.png?theme=light&md=0&fontSize=75px&images=https%3A%2F%2Fassets.vercel.com%2Fimage%2Fupload%2Ffront%2Fassets%2Fdesign%2Fnextjs-black-logo.svg`}
+            />
+            <meta name="og:title" content={siteTitle} />
+            <meta name="twitter:card" content="summary_large_image" />
+          </Head>
+          <main className="flex items-left justify-left h-screen flex-col">
+            <div className={styles.buttonContainer}>
+              <Button />
+            </div>
+          </main>
+          <header className={styles.header}>
+            {home ? (
+              <>
+                <Image
+                  priority
+                  src="/images/profile.png"
+                  className={utilStyles.borderCircle}
+                  height={144}
+                  width={144}
+                  alt=""
+                />
+                <h1 className={utilStyles.heading2Xl}>{name}</h1>
+              </>
+            ) : (
+              <>
+                <Link href="/">
+                  <Image
+                    priority
+                    src="/images/profile.png"
+                    className={utilStyles.borderCircle}
+                    height={108}
+                    width={108}
+                    alt=""
+                  />
+                </Link>
+                <h2 className={utilStyles.headingLg}>
+                  <Link href="/" className={utilStyles.colorInherit}>
+                    {name}
+                  </Link>
+                </h2>
+              </>
+            )}
+          </header>
+          <main>{children}</main>
+          {!home && (
+            <div className={styles.backToHome}>
+              <Link href="/">
+                <IconButton className="bg-gray-800 dark:bg-gray-100 hover:bg-gray-600 dark:hover:bg-gray-300 transition-all duration-100 text-white dark:text-gray-800 rounded-full p-2">
+                  <HomeIcon />
+                </IconButton>
               </Link>
-            </h2>
-          </>
-        )}
-      </header>
-      <main>{children}</main>
-      {!home && (
-        <div className={styles.backToHome}>
-          <Link href="/">
-            <IconButton
-              className='bg-gray-800 dark:bg-gray-100 hover:bg-gray-600 dark:hover:bg-gray-300 transition-all duration-100 text-white dark:text-gray-800 rounded-full p-2'>
-        <HomeIcon />
-      </IconButton></Link>
+            </div>
+          )}
         </div>
-      )}
-    </div>
-    </ScrollAnimation>
+      </ScrollAnimation>
     </motion.div>
   );
 }
